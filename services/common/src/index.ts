7ab4265edc4861785e1e5ffab94e4cd5e89e7f18
Alberto Soler <asoler@topdoctors.es>
2020-08-29 16:07:54 +0200

    "Updates"

--- services/common/src/index.ts
+++ services/common/src/index.ts
@@ -1,3 +1,7 @@
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
+
+
+export * from './events/event-created-event'
+export * from './events/user-created-event'
