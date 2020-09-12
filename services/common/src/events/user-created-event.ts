acd84b946ce0489b0ad702695fb1fe8d1735832d
Alberto Soler <asoler@topdoctors.es>
2020-09-12 16:11:36 +0200

    "Updates"

--- services/common/src/events/user-created-event.ts
+++ services/common/src/events/user-created-event.ts
@@ -1,9 +1,10 @@
 import { Subjects } from './subjects'
-
+import mongoose from 'mongoose'
 
 export interface UserCreatedEvent {
   subject: Subjects.UserCreated
   data: {
     id: string,
+    phoneID: mongoose.Schema.Types.ObjectId
   }
 }
